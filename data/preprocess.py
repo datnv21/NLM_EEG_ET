ec392968eaf8e60a6013919c19c1fc2892680591
DatNguyen <datnv21@github.com>
2021-06-18 23:35:23 +0700

    Feat: Training a simple nlm

---
+++ data/preprocess.py
@@ -0,0 +1,49 @@
+import os
+import sys
+sys.path.append("utils")
+import json
+
+from constant import VN_RE, SEQUENCE_LENGTH
+
+
+TEXT_PATH = "text_timestamp.json"
+
+
+def create_corpus():
+    with open(TEXT_PATH, "r") as f:
+        data = json.load(f)
+        f.close()
+
+    text = data["text"]
+    corpus = " ".join(text)
+    corpus = corpus.lower()
+    
+    return corpus
+
+
+def create_seq(corpus):
+    length = SEQUENCE_LENGTH
+    sequences = list()
+
+    for i in range(length, len(corpus)):
+        seq = corpus[i-length:i+1]
+        sequences.append(seq)
+
+    return sequences
+
+
+def prepare():
+    corpus = create_corpus()
+    chars = sorted(list(set(corpus)))
+    mapping = dict((c,i) for i, c in enumerate(chars))
+    sequences = create_seq(corpus)
+
+    sequences_encoded = list()
+    for line in sequences:
+        encoded_seq = [mapping[char] for char in line]
+        sequences_encoded.append(encoded_seq)
+
+    return sequences_encoded, mapping
+
+if __name__=="__main__":
+    prepare()
---
+++ inference/inference.py
@@ -0,0 +1,46 @@
+import os
+import sys
+sys.path.append("models")
+sys.path.append("utils")
+sys.path.append("data")
+
+from model import NLMBasic
+from preprocess import prepare
+from keras.preprocessing.sequence import pad_sequences
+from constant import SEQUENCE_LENGTH
+
+
+model_weight = "checkpoints/model.h5"
+
+
+def generate_seq(model, mapping, seq_length, seed_text, n_chars):
+	in_text = seed_text
+	for _ in range(n_chars):
+		encoded = [mapping[char] for char in in_text]
+		encoded = pad_sequences([encoded], maxlen=seq_length, truncating='pre')
+		yhat = model.predict_classes(encoded, verbose=0)
+		out_char = ''
+		for char, index in mapping.items():
+			if index == yhat:
+				out_char = char
+				break
+		in_text += char
+	return in_text
+
+
+def inference():
+    sequences_encoded, mapping = prepare()
+    vocab_size = len(mapping)
+    nlm_basic = NLMBasic(vocab_size)
+    model = nlm_basic.build_model()
+    model.load_weights(model_weight, by_name=True, skip_mismatch=True)
+
+    seed_text = "tôi m"
+    result = generate_seq(model, mapping, SEQUENCE_LENGTH, seed_text, len(mapping))
+    print(result)
+
+    return 1
+
+
+if __name__=="__main__":
+    inference()
